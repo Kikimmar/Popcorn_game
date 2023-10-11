f41de0027f0fc8309b9d54123b1321ed51748da2
Kikimmar <kikimmar.m@gmail.com>
2023-10-11 10:48:49 +0700

    Добавить анимацию активного кирпича ч.2

--- Popcorn/Popcorn/Active_Brick.h
+++ Popcorn/Popcorn/Active_Brick.h
@@ -3,17 +3,37 @@
 #include "Config.h"
 
 //------------------------------------------------------------------------------------------------------------
+enum EBrick_Type
+{
+	EBT_None,
+	EBT_Red,
+	EBT_Blue
+};
+//------------------------------------------------------------------------------------------------------------
 class AActive_Brick
 {
 public:
-	AActive_Brick();
+	AActive_Brick(EBrick_Type brick_type);
 
 	void Act(HWND Hwnd);
 	void Draw(HDC hdc, RECT& paint_area);
+	
+	static void SetupColors();
 
+private:
+	EBrick_Type Brick_Type;
 	int Fade_Step;
 	RECT Brick_Rect;
 
+	static unsigned char Get_Fading_Channel(unsigned char color, unsigned char bg_color, int step);
+	static void Get_Fading_Color(const AColor& color, int step, HPEN& pen, HBRUSH& brush);
+	
 	static const int Max_Fade_Step = 40;
+
+	static HPEN Fading_Red_Brick_Pens[Max_Fade_Step];
+	static HBRUSH Fading_Red_Brick_Brushes[Max_Fade_Step];
+	static HPEN Fading_Blue_Brick_Pens[Max_Fade_Step];
+	static HBRUSH Fading_Blue_Brick_Brushes[Max_Fade_Step];
+
 };
 //------------------------------------------------------------------------------------------------------------
--- Popcorn/Popcorn/Engine.cpp
+++ Popcorn/Popcorn/Engine.cpp
@@ -3,7 +3,7 @@
 //AsEngine
 //------------------------------------------------------------------------------------------------------------
 AsEngine::AsEngine()
-	: Hwnd(0), BG_Pen(0), BG_Brush(0)
+	: Hwnd(0)
 {
 }
 //------------------------------------------------------------------------------------------------------------
@@ -11,8 +11,8 @@ void AsEngine::Init_Engine(HWND hwnd)
 {// Настройка игры при старте
 
 	Hwnd = hwnd;
-
-	AsConfig::Create_Pen_Brush(15, 63, 31, BG_Pen, BG_Brush);
+	
+	AActive_Brick::SetupColors();
 
 	Ball.Init();
 	Level.Init();
@@ -29,7 +29,7 @@ void AsEngine::Draw_Frame(HDC hdc, RECT& paint_area)
 
 	Level.Draw(Hwnd, hdc, paint_area);
 
-	Platform.Draw(hdc, BG_Pen, BG_Brush, paint_area);
+	Platform.Draw(hdc, paint_area);
 
 	//int i;
 
@@ -39,9 +39,9 @@ void AsEngine::Draw_Frame(HDC hdc, RECT& paint_area)
 	//	Draw_Brick_Letter(hdc, 20 + i * Cell_Width * AsConfig::Global_Scale, 130, EBT_Red, ELT_O, i);
 	//}
 
-	Ball.Draw(hdc, paint_area, BG_Pen, BG_Brush);
+	Ball.Draw(hdc, paint_area);
 
-	Border.Draw(hdc, paint_area, BG_Pen, BG_Brush);
+	Border.Draw(hdc, paint_area);
 }
 //------------------------------------------------------------------------------------------------------------
 int AsEngine::On_Key_Down(EKey_Type key_type)
